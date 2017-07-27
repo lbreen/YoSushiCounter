bcac7875f6556a324f1344a60eb62a172b8c07b7
Luke Breen <l.breen1@outlook.com>
2017-07-27 12:19:03 +0100

    Refactored dishBtn code into styles.xml

--- app/src/main/java/com/example/lbreen/yosushicounter/MainActivity.java
+++ app/src/main/java/com/example/lbreen/yosushicounter/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lbreen.yosushicounter;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -44,4 +46,5 @@ public class MainActivity extends AppCompatActivity {
     public void review() {
         // Open review tab
     }
+
 }
