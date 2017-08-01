ed29eefaeb07cd18d8f800c042a92c39d75485ae
Luke Breen <l.breen1@outlook.com>
2017-08-01 16:52:10 +0100

    Information is passed in a hashmap between activities, with an Intent - need to iterate over the hashmap to retrieve information

--- app/src/main/java/com/example/lbreen/yosushicounter/MainActivity.java
+++ app/src/main/java/com/example/lbreen/yosushicounter/MainActivity.java
@@ -1,26 +1,53 @@
 package com.example.lbreen.yosushicounter;
 
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import static android.R.id.message;
-import static com.example.lbreen.yosushicounter.R.styleable.View;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setTempFile(Context context);
         setContentView(R.layout.activity_main);
     }
 
     Double totalValue = 0.0;
+
+    HashMap dishQuantities = new HashMap();
+
+
+//    public void getTempFile(){
+//        String fileName = "dish_quantities";
+//        File file = null;
+//        try {
+//           file = File.createTempFile(fileName, null, context.getCacheDir());
+//        } catch (IOException e){
+//
+//        }
+//
+//        String string = "hello world";
+//
+//        try {
+//            FileOutputStream outputStream;
+//            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+//            outputStream.write(string.getBytes());
+//            outputStream.close();
+//        } catch (IOException e){
+//
+//        }
+//
+//        return file;
+//    }
+
 
     /**
      * Called when the user taps the Calculate button
@@ -28,8 +55,17 @@ public class MainActivity extends AppCompatActivity {
     public void addValue(View view) {
         Button b = (Button) view;
         String[] buttonText = b.getText().toString().split("£"); // Retrieve the btn text and remove '£' symbol => [ , #{value}]
-        Double buttonValue = Double.parseDouble(buttonText[1]); // Convert btn value from string to double (float)
-        totalValue += buttonValue; // Increment the total value
+        Double dishValue = Double.parseDouble(buttonText[1]); // Retrieve the dish value from the button text
+
+//        String buttonTag = b.getTag().toString();
+//        if (buttonTag == null) {
+//            dishQuantities.put(buttonTag, 1);
+//        } else {
+//            Integer currentQuantity = dishQuantities.get(buttonTag);
+//            currentQuantity += 1;
+//            dishQuantities.put(buttonTag, currentQuantity);
+//        }
+        totalValue += dishValue; // Increment the total value
         displayValue(totalValue); // Display value to screen
     }
 
@@ -45,11 +81,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void review(View view) {
-        Intent intent = new Intent(this, ReviewActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        Intent intent = new Intent(this, ReviewActivity.class); // Initialise a new ReviewActivity instance
+        dishQuantities.put("green", 2);
+        dishQuantities.put("orange", 3);
+        dishQuantities.put("blue", 4);
+        dishQuantities.put("purple", 5);
+        intent.putExtra("dishQuantitiesHash", dishQuantities);
+        startActivity(intent); // Start ReviewActivity
     }
 
 }
--- app/src/main/java/com/example/lbreen/yosushicounter/ReviewActivity.java
+++ app/src/main/java/com/example/lbreen/yosushicounter/ReviewActivity.java
@@ -3,7 +3,14 @@ package com.example.lbreen.yosushicounter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ReviewActivity extends AppCompatActivity {
 
@@ -13,11 +20,18 @@ public class ReviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_review);
     }
 
-//    // Get the Intent that started this activity and extract the string
-//    Intent intent = getIntent();
-//    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-//
-//    // Capture the layout's TextView and set the string as its text
-//    TextView textView = (TextView) findViewById(R.id.textView);
-//    textView.setText(message);
+    public void changeText(View view) {
+        Intent intent = getIntent();
+        HashMap dishQuantities = (HashMap<String, Integer>) intent.getSerializableExtra("dishQuantitiesHash");
+        Iterator<Map.Entry<String, Integer>> entries = dishQuantities.entrySet().iterator();
+
+        while (entries.hasNext()) {
+            Map.Entry<String, Integer> entry = entries.next();
+            Integer value = entry.getValue();
+            TextView testText = (TextView) findViewById(R.id.testText);
+            testText.setText(Integer.toString(value));
+        }
+
+    }
+
 }
